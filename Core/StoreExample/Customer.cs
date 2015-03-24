306ccd8bd6fb67639428a75f73812cff2bcd3e4e
Steven van der Merwe <stevenvdmerwe@gmail.com>
2015-03-24 23:30:16 +0200

    First Commit

---
+++ Core/StoreExample/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.StoreExample
+{
+    public class Customer
+    {
+        public DateTime? DateOfFirstPurchase { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsVeteran { get; set; }
+    }
+}
